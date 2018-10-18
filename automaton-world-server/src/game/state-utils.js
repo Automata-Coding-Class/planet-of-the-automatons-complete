30f9d14e273a105541babce1f20a93b88c602257
Rick Walters <rick@yonderwindow.com>
2018-10-18 16:37:57 -0400

    state-utils moved over from PoC project, and tests written for all functions

---
+++ automaton-world-server/src/game/state-utils.js
@@ -0,0 +1,95 @@
+const getRandomIntegerInRange = function (min, max) {
+  if (min > max) {
+    const temp = max;
+    max = min;
+    min = temp;
+  }
+  const rangeSize = max - min;
+  if (rangeSize === 0) {
+    return min;
+  } else {
+    return Math.floor(Math.random() * rangeSize) + min;
+  }
+};
+module.exports.getRandomIntegerInRange = getRandomIntegerInRange;
+
+const getRowAndColumnFromIndex = function (columns, index) {
+  const r = Math.floor(index / columns);
+  const c = index % columns;
+  return {row: r, col: c};
+};
+module.exports.getRowAndColumnFromIndex = getRowAndColumnFromIndex;
+
+module.exports.getIndexFromRowAndColumn = function(columns, row, col) {
+  return row * columns + col;
+};
+
+module.exports.getRelativeIndex = function(index, rows, columns, direction) {
+  let newIndex = -1;
+  switch(direction) {
+    case 'up':
+      newIndex =  Math.max(index - columns, -1);
+      break;
+    case 'right':
+      newIndex =  index + 1 < rows * columns ? index + 1 : -1;
+      break;
+    case 'down':
+      newIndex =  index + columns < rows * columns ? index + columns : -1;
+      break;
+    case 'left':
+      newIndex =  Math.max(index - 1, -1);
+      break;
+    default:
+      newIndex = index;
+      break;
+  }
+  return newIndex;
+};
+
+const getFilledIndices = function (sparseArray) {
+  return sparseArray.reduce((acc, item, i) => {
+    if (item !== undefined) acc.push(i);
+    return acc;
+  }, []);
+};
+module.exports.getFilledIndices = getFilledIndices;
+
+const getEmptyIndices = function (sparseArray) {
+  const emptyIndices = [];
+  for (let i = 0; i < sparseArray.length; i++) {
+    if (sparseArray[i] === undefined) emptyIndices.push(i);
+  }
+  return emptyIndices;
+};
+module.exports.getEmptyIndices = getEmptyIndices;
+
+const isSparseArrayFull = function (sparseArray) {
+  return getFilledIndices(sparseArray).length >= sparseArray.length;
+};
+module.exports.isSparseArrayFull = isSparseArrayFull;
+
+module.exports.getUnoccupiedSlot = function (sparseArray) {
+  if (sparseArray.length > 0 && isSparseArrayFull(sparseArray)) {
+    return null;
+  } else {
+    const availableSlots = getEmptyIndices(sparseArray);
+    return availableSlots[getRandomIntegerInRange(0, availableSlots.length)];
+  }
+};
+
+module.exports.getNeighbourCellValues = function(sparseArray, cols, index, edgeValue) {
+  if(edgeValue === undefined) edgeValue = { type: 'edge' };
+  const rows = Math.ceil(sparseArray.length / cols);
+  // console.log(`rows=${rows}; cols=${cols}; index=${index}`);
+  const top = Math.floor(index/cols) === 0 ? edgeValue :
+    sparseArray[index-cols] !== undefined ? sparseArray[index-cols] : null;
+  const right = index % cols === cols - 1 ? edgeValue :
+    sparseArray[index+1] !== undefined ? sparseArray[index+1] : null;
+  const bottom = Math.floor(index / cols) === rows - 1 ? edgeValue:
+    sparseArray[index+cols] !== undefined ? sparseArray[index+cols] : null;
+  const left = index % cols === 0 ? edgeValue :
+    sparseArray[index-1] !== undefined ? sparseArray[index-1] : null;
+  return {top: top, right: right, bottom: bottom, left: left};
+};
+
+
---
+++ automaton-world-server/src/game/state-utils.test.js
@@ -0,0 +1,138 @@
+const {getRandomIntegerInRange, getRowAndColumnFromIndex, getIndexFromRowAndColumn, getRelativeIndex, getFilledIndices, getEmptyIndices, getUnoccupiedSlot, getNeighbourCellValues} = require('./state-utils');
+
+
+describe('state-utils', () => {
+  // grid for positional tests on non-empty grid:
+  // ┌───┬───┬───┬───┬───┐
+  // │   │   │   │   │   │
+  // ├───┼───┼───┼───┼───┤
+  // │   │ p │   │ o │   │
+  // ├───┼───┼───┼───┼───┤
+  // │ o │   │   │   │ o │
+  // ├───┼───┼───┼───┼───┤
+  // │   │ a │ t │ p │   │
+  // └───┴───┴───┴───┴───┘
+
+  const grid_4x5 = new Array(25);
+  grid_4x5[6] = {type: 'player'};
+  grid_4x5[8] = {type: 'object'};
+  grid_4x5[10] = {type: 'object'};
+  grid_4x5[14] = {type: 'object'};
+  grid_4x5[16] = {type: 'asset'};
+  grid_4x5[17] = {type: 'target'};
+  grid_4x5[18] = {type: 'player'};
+  const grid_4x5_cols = 5;
+  const grid_4x5_filledIndices = [6, 8, 10, 14, 16, 17, 18];
+  const grid_4x5_emptyIndices = [0, 1, 2, 3, 4, 5, 7, 9, 11, 12, 13, 15, 19, 20, 21, 22, 23, 24];
+
+  describe('getRandomIntegerInRange', () => {
+    test('result is integer', () => {
+      const result = getRandomIntegerInRange(0, 10);
+      expect(result - Math.floor(result)).toBe(0);
+    });
+    test('result is in provided range', () => {
+      const min = 5, max = 10, testCount = (max - min) * 100;
+      for (let i = 0; i < testCount; i++) {
+        let randomInt = getRandomIntegerInRange(min, max);
+        expect(randomInt).toBeGreaterThanOrEqual(min);
+        expect(randomInt).toBeLessThanOrEqual(max);
+      }
+    });
+    test('result of zero-width range is correct', () => {
+      const testInt = 5;
+      expect(getRandomIntegerInRange(testInt, testInt)).toBe(testInt);
+    });
+    test('result of reversed range is correct', () => {
+      const min = 5, max = 10;
+      let randomInt = getRandomIntegerInRange(max, min);
+      expect(randomInt).toBeGreaterThanOrEqual(min);
+      expect(randomInt).toBeLessThanOrEqual(max);
+    });
+  });
+
+  describe('getRowAndColumnFromIndex', () => {
+    test('returns correct values, basic case', () => {
+      expect(getRowAndColumnFromIndex(grid_4x5_cols, 13)).toEqual({row: 2, col: 3});
+    });
+  })
+
+  describe('getIndexFromRowAndColumn', () => {
+    test('returns correct values, basic case', () => {
+      expect(getIndexFromRowAndColumn(grid_4x5_cols, 2, 3)).toEqual(13);
+    });
+  });
+
+  describe('getRelativeIndex', () => {
+    test('check results of move in cardinal direction', () => {
+      expect(getRelativeIndex(7, 4, grid_4x5_cols, 'up')).toBe(2);
+      expect(getRelativeIndex(7, 4, grid_4x5_cols, 'right')).toBe(8);
+      expect(getRelativeIndex(7, 4, grid_4x5_cols, 'down')).toBe(12);
+      expect(getRelativeIndex(7, 4, grid_4x5_cols, 'left')).toBe(6);
+      expect(getRelativeIndex(7, 4, grid_4x5_cols, 'foobar')).toBe(7);
+    });
+  });
+
+  describe('getFilledIndices', () => {
+    test('check for length and contents of returned array', () => {
+      expect(getFilledIndices(grid_4x5)).toEqual(grid_4x5_filledIndices);
+    })
+  });
+
+  describe('getEmptyIndices', () => {
+    test('check for length and contents of returned array', () => {
+      expect(getEmptyIndices(grid_4x5)).toEqual(grid_4x5_emptyIndices);
+    })
+  });
+
+  describe('getUnoccupiedSlot', () => {
+    test('check that result is not an occupied index', () => {
+      const testSlotIndex = getUnoccupiedSlot(grid_4x5);
+      expect(grid_4x5_filledIndices.some(elem => elem === testSlotIndex)).toBe(false);
+    })
+  });
+
+  describe('getNeighbourCellValues', () => {
+    test('neighbour cell values on an empty 3x3 grid', () => {
+      const sparseArray = new Array(9);
+      const cols = 3;
+      // first row
+      expect(getNeighbourCellValues(sparseArray, cols, 0))
+        .toEqual({top: {type: 'edge'}, right: null, bottom: null, left: {type: 'edge'}});
+      expect(getNeighbourCellValues(sparseArray, cols, 1))
+        .toEqual({top: {type: 'edge'}, right: null, bottom: null, left: null});
+      expect(getNeighbourCellValues(sparseArray, cols, 2))
+        .toEqual({top: {type: 'edge'}, right: {type: 'edge'}, bottom: null, left: null});
+      // second row
+      expect(getNeighbourCellValues(sparseArray, cols, 3))
+        .toEqual({top: null, right: null, bottom: null, left: {type: 'edge'}});
+      expect(getNeighbourCellValues(sparseArray, cols, 4))
+        .toEqual({top: null, right: null, bottom: null, left: null});
+      expect(getNeighbourCellValues(sparseArray, cols, 5))
+        .toEqual({top: null, right: {type: 'edge'}, bottom: null, left: null});
+      // // third row
+      expect(getNeighbourCellValues(sparseArray, cols, 6))
+        .toEqual({top: null, right: null, bottom: {type: 'edge'}, left: {type: 'edge'}});
+      expect(getNeighbourCellValues(sparseArray, cols, 7))
+        .toEqual({top: null, right: null, bottom: {type: 'edge'}, left: null});
+      expect(getNeighbourCellValues(sparseArray, cols, 8))
+        .toEqual({top: null, right: {type: 'edge'}, bottom: {type: 'edge'}, left: null});
+    });
+
+
+    test('check contents of cells at cardinal points', () => {
+      const boundaryObject = {type: 'border'};
+      expect(getNeighbourCellValues(grid_4x5, grid_4x5_cols, 3, boundaryObject))
+        .toEqual({top: boundaryObject, right: null, bottom: {type: 'object'}, left: null});
+      expect(getNeighbourCellValues(grid_4x5, grid_4x5_cols, 7, boundaryObject))
+        .toEqual({top: null, right: {type: 'object'}, bottom: null, left: {type: 'player'}});
+      expect(getNeighbourCellValues(grid_4x5, grid_4x5_cols, 11, boundaryObject))
+        .toEqual({
+          top: {type: 'player'},
+          right: null,
+          bottom: {type: 'asset'},
+          left: {type: 'object'}
+        });
+    });
+
+  });
+});
